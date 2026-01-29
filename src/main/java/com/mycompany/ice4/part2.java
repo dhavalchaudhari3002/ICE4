37e8262ece748bbb1b4d57c00c4196e742c3eaf6
Dhaval Chaudhari <dc7112837@gmail.com>
2026-01-28 21:37:50 -0500

    comment added

---
+++ src/main/java/com/mycompany/ice4/part2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ice4;
+
+/**
+ *
+ * @author dgcha
+ */
+public class part2 {
+    
+    // part2 hello
+    
+    
+}
